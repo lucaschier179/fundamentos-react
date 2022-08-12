4546ccb2f5f0bbd3a989d728b4e3c4f78c9654be
lucaschier <ens-lucasschier@uniguacu.edu.br>
2022-08-12 10:21:30 -0300

    aula 22

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import Card from "./components/layout/Card";
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -8,13 +9,24 @@ import Fragmento from './components/basicos/Fragmento'
 export default () => (
     <div id="app">
         <h1>Fundamentos React (Arrow _)</h1>
-        <Aleatorio min={1} max={100} />
-        <Fragmento/>
-        <ComParametro
-            titulo="Nota do Aluno"
-            aluno="Lucas Marcelo"  
-            nota={9.7}
-        />
-        <Primeiro></Primeiro>
+
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={1} max={100} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro
+                titulo="Nota do Aluno"
+                aluno="Lucas Marcelo"  
+                nota={9.7}
+            />
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
     </div>
 );
