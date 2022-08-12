2a2801a6411195bba1263fe2e9179ac076310592
lucaschier <ens-lucasschier@uniguacu.edu.br>
2022-08-12 07:43:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react"
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+
 
 export default function App(props) {
     return (
         <div id='app'>
-        <Fragmento />
+        <Fragmento/>
+        
         <ComParametro 
             titulo="Nota do Aluno"
             aluno="Lucas"  
-            nota={9.7}
-        />
+            nota={9.7}/>
         <Primeiro></Primeiro>
     </div>,
     )
