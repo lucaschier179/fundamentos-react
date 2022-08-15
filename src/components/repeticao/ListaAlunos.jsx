98bf43aaf7fd17c744e38e04432776347d86d27d
lucaschier <ens-lucasschier@uniguacu.edu.br>
2022-08-15 10:30:57 -0300

    update aula 27

--- src/components/repeticao/ListaAlunos.jsx
+++ src/components/repeticao/ListaAlunos.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     const alunosJSX = alunos.map(aluno =>{
         return (
             <li key={aluno.id}>
-                {aluno.id}) {aluno.nome} -> {aluno.nota}
+                {aluno.id}{')'} {aluno.nome} {'='} {aluno.nota}
             </li>
         );
     });
