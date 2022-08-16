b3771960590206aa13f37d51f6af94cdf3f299c5
lucaschier <ens-lucasschier@uniguacu.edu.br>
2022-08-16 13:26:07 -0300

    aula 35
    Componentes com estado

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export default props => {
     const cb = props.quandoClicar
+    const min = 1
+    const max = 150
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>Filho</div>
-            <button onClick={_ => cb('Lucas', 18, true)}>
+            <button onClick={_ => cb('Lucas', gerarIdade(), gerarNerd())}>
                 Fornecer Informações
             </button>
         </div>
